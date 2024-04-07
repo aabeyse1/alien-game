6faa5e63f39829e14f0fbce6854ff9384de51162
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2024-04-07 13:40:33 -0500

    wasd

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -25,29 +25,29 @@ public class Movement : MonoBehaviour
     {
         Vector2 movement = Vector2.zero;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow)) lastUpPress = Time.time;
-        if (Input.GetKeyDown(KeyCode.DownArrow)) lastDownPress = Time.time;
-        if (Input.GetKeyDown(KeyCode.RightArrow)) lastRightPress = Time.time;
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) lastLeftPress = Time.time;
+        if (Input.GetKeyDown(KeyCode.W)) lastUpPress = Time.time;
+        if (Input.GetKeyDown(KeyCode.S)) lastDownPress = Time.time;
+        if (Input.GetKeyDown(KeyCode.D)) lastRightPress = Time.time;
+        if (Input.GetKeyDown(KeyCode.A)) lastLeftPress = Time.time;
 
         float latestKeyPressTime = Mathf.Max(lastUpPress, lastDownPress, lastRightPress, lastLeftPress);
 
-        if (latestKeyPressTime == lastRightPress && Input.GetKey(KeyCode.RightArrow))
+        if (latestKeyPressTime == lastRightPress && Input.GetKey(KeyCode.D))
         {
             movement.x = speed;
             animator.SetInteger("Direction", 3); 
         }
-        else if (latestKeyPressTime == lastLeftPress && Input.GetKey(KeyCode.LeftArrow))
+        else if (latestKeyPressTime == lastLeftPress && Input.GetKey(KeyCode.A))
         {
             movement.x = -speed;
             animator.SetInteger("Direction", 4); 
         }
-        else if (latestKeyPressTime == lastUpPress && Input.GetKey(KeyCode.UpArrow))
+        else if (latestKeyPressTime == lastUpPress && Input.GetKey(KeyCode.W))
         {
             movement.y = speed;
             animator.SetInteger("Direction", 1); 
         }
-        else if (latestKeyPressTime == lastDownPress && Input.GetKey(KeyCode.DownArrow))
+        else if (latestKeyPressTime == lastDownPress && Input.GetKey(KeyCode.S))
         {
             movement.y = -speed;
             animator.SetInteger("Direction", 2); 
