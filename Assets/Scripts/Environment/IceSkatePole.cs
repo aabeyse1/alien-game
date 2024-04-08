621c5763734c6bd34cdc549ab4aa24e1abaf8629
lucyrubin <lucyjr111@gmail.com>
2024-04-07 20:20:35 -0500

    Added ice skate that is knocked down by rakerake

---
+++ Assets/Scripts/Environment/IceSkatePole.cs
@@ -0,0 +1,94 @@
+
+// Author(s): Lucy Rubin
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSkatePole : MonoBehaviour
+{
+    private InteractIcon interactIcon;
+    private bool playerIsNear = false;
+
+    private Animator animator;
+
+    [SerializeField] GameObject iceSkatePickUpItem;
+
+    [SerializeField] GameObject iceSkateVisual;
+
+    private void Awake()
+    {
+        interactIcon = GetComponentInChildren<InteractIcon>();
+        animator = GetComponent<Animator>();
+    }
+    private void OnEnable()
+    {
+        GameEventsManager.instance.inputEvents.onSubmitPressed += SubmitPressed;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.inputEvents.onSubmitPressed -= SubmitPressed;
+
+    }
+
+    private void SubmitPressed()
+    {
+        // Do nothing if the skate has already fallen
+        if(!iceSkateVisual.activeInHierarchy) {
+            return;
+        }
+        if (playerIsNear)
+        {
+            bool playerHasRakeRake = InventoryManager.isItemInInventory("RakeRake");
+            if (playerHasRakeRake)
+            {
+                // knock down ice skate
+                StartCoroutine(FallingAnimationCoroutine());
+
+
+            }
+            else
+            {
+                // unable to get the skate down - hit your hand on it
+                // TODO: Run a hitting animation
+                DialogueManager.instance.RunDialogueNode("Punch_Pole");
+
+            }
+
+
+        }
+
+    }
+
+    private IEnumerator FallingAnimationCoroutine()
+    {
+        animator.enabled = true;
+        animator.Play("IceSkateFall", -1, 0f);
+        // Wait for the animation to reach its end
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
+        animator.enabled = false;
+        iceSkatePickUpItem.SetActive(true);
+        iceSkateVisual.SetActive(false);
+        
+
+    }
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        
+
+        if (otherCollider.CompareTag("Player"))
+        {
+            interactIcon.SetState(active: true, locked: false);
+            playerIsNear = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+         
+        if (otherCollider.CompareTag("Player"))
+        {
+            playerIsNear = false;
+            interactIcon.SetState(active: false, locked: false);
+        }
+    }
+}
