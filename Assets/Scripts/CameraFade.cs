6ed79b08c519cd957e92cc21d06f33679bf842cc
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2024-04-15 08:43:38 -0500

    Fixed overlay issue

--- Assets/Scripts/CameraFade.cs
+++ Assets/Scripts/CameraFade.cs
@@ -15,10 +15,12 @@ public class CameraFade : MonoBehaviour
 
     private IEnumerator FadeOutAndIn(Action onFullyFaded)
     {
+        fadeOverlay.gameObject.SetActive(true);
         yield return StartCoroutine(FadeTo(1));  // Fade to black
         onFullyFaded?.Invoke();  // Call the callback to change the area when fully black
         yield return new WaitForSeconds(0.5f);  // Wait at fully black for half a second
         yield return StartCoroutine(FadeTo(0));  // Fade back to scene
+        fadeOverlay.gameObject.SetActive(false);
     }
 
     private IEnumerator FadeTo(float targetAlpha)
